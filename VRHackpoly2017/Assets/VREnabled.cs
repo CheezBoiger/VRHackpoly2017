42c2bd76e07ad66a06f659f98daf8468b591e37c
CheezBoiger <alexander.garcia18@yahoo.com>
2017-02-11 15:02:00 -0800

    Created Test VR Scene

---
+++ VRHackpoly2017/Assets/VREnabled.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VR;
+using UnityEngine;
+
+public class VREnabled : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.V)) {
+			VRSettings.enabled = !VRSettings.enabled;
+			Debug.Log("Changed VRSettings.enabled to: " + VRSettings.enabled);
+		}
+	}
+}
